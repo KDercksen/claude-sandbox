8bf3e7997f9cdb171b25b4b6b1d9efc0ba5d3849
KDercksen <mail@koendercksen.com>
2026-03-27 21:23:47 +0100

    chore: remove TypeScript codebase

--- test/commands/ls.test.ts
+++
@@ -1,9 +0,0 @@
-import {runCommand} from '@oclif/test'
-import {expect} from 'chai'
-
-describe('ls', () => {
-  it('runs ls without error', async () => {
-    const {error} = await runCommand('ls')
-    expect(error).to.be.undefined
-  })
-})
